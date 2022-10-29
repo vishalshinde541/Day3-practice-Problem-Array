f11e286d1955fcbf4781a4a901434122dfd9c27f
vishalshinde541 <vishalshinde541@gmail.com>
2022-10-30 00:44:10 +0530

    Reverse array

--- src/com/Bridgelabz/Array/Elements_On_Even_And_Odd_Position.java
+++ src/com/Bridgelabz/Array/Elements_On_Even_And_Odd_Position.java
@@ -3,7 +3,7 @@ package com.Bridgelabz.Array;
 public class Elements_On_Even_And_Odd_Position {
 	public static void main(String[] args) {
 
-		int[] ar = { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+		int[] ar = new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 
 		System.out.println("Elements of given array present on even position: ");
 
---
+++ src/com/Bridgelabz/Array/Reverse_Array.java
@@ -0,0 +1,19 @@
+package com.Bridgelabz.Array;
+
+public class Reverse_Array {
+	public static void main(String[] args) {
+		int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		System.out.println("Original array: ");
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		System.out.println("Array in reverse order: ");
+
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
